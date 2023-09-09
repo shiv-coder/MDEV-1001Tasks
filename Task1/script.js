f14cd0603a88e5d6646961b52b9ad869a25cb5bd
Shivali Dhaka <56281084+shiv-coder@users.noreply.github.com>
2023-09-08 21:36:39 -0400

    Update script.js

--- Task1/script.js
+++ Task1/script.js
@@ -1,7 +1,1 @@
-fetch('https://api.giphy.com/v1/gifs/random?api_key=bwBAcjO5j2Cec47TG7flBCn7Gdw57Fo8&tag=squirrel&rating=g')
-.then(function(response) {
-	return response.json();
-})
-.then(function(jsonData) {
-	console.log(jsonData);
-})
+console.log("This is your first task");
